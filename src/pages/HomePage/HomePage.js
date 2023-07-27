788b361aea5ae7f2a7788d38caa2fe51e681517b
IrynaShashko <irishkashashko@gmail.com>
2023-07-27 12:33:00 +0300

    fix

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -35,7 +35,7 @@ const HomePage = () => {
           >
             <IconContext.Provider
               value={{
-                size: "25px",
+                size: "18px",
                 color: "#fff",
               }}
             >
