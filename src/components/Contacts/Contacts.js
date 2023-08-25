c06a243c45d05ff22d971eee6507f46fa8aaa1a8
IrynaShashko <irishkashashko@gmail.com>
2023-08-25 12:26:17 +0300

    add google map

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -2,15 +2,32 @@ import { Container } from "../../App.styled";
 import ConnectionButtons from "../ConnectionButton/ConnectionButton";
 import {
   ContactsContainer,
+  GridContainer,
+  Grid1,
+  Grid2,
   Title,
+  Location,
 } from "../../components/Contacts/Contacts.styled";
 
 const Contacts = () => {
   return (
     <ContactsContainer id="contacts">
       <Container>
-        <Title>Контактна інформація:</Title>
-        <ConnectionButtons background={"#fff"} color={"#696969"} />
+        <GridContainer>
+          <Grid1>
+            <Title>Контактна інформація:</Title>
+            <ConnectionButtons background={"#fff"} color={"#696969"} />
+          </Grid1>
+          <Grid2>
+            <Location
+              title="Location"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2540.273356384819!2d30.35803687630494!3d50.4546341870629!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cc9a797be691%3A0x6529cfbd9c11e34!2zMTIx0JAsINC_0YDQvtGB0L_QtdC60YIg0J_QtdGA0LXQvNC-0LPQuCwgMTE10JAsINCa0LjRl9CyLCAwMjAwMA!5e0!3m2!1suk!2sua!4v1692952327783!5m2!1suk!2sua"
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></Location>
+          </Grid2>
+        </GridContainer>
       </Container>
     </ContactsContainer>
   );
--- src/components/Contacts/Contacts.styled.js
+++ src/components/Contacts/Contacts.styled.js
@@ -6,10 +6,45 @@ export const ContactsContainer = styled.div`
   padding: 50px 20px;
 `;
 
+export const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 0.5fr 1fr;
+  gap: 10px 10px;
+  grid-template-areas:
+    "grid1"
+    "grid2";
+  @media screen and (min-width: 1440px) {
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr;
+    grid-template-areas: "grid1 grid2";
+  }
+`;
+
+export const Grid1 = styled.div`
+  grid-area: "grid1";
+  align-self: center;
+`;
+
+export const Grid2 = styled.div`
+  grid-area: "grid2";
+`;
+
 export const Title = styled.h3`
   color: #fff;
   font-size: 30px;
   text-transform: uppercase;
   font-weight: lighter;
   margin-bottom: 20px;
+`;
+
+export const Location = styled.iframe`
+  width: 320px;
+  height: 300px;
+  border-radius: 8px;
+  border: none;
+  @media screen and (min-width: 768px) {
+    width: 500px;
+    height: 300px;
+  }
 `;
